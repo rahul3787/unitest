995efaf35e3443b2ea8ff25023b5e4957c9b1bdb
rahul3787 <39632258+rahul3787@users.noreply.github.com>
2020-10-30 13:30:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Maps from "./component/Maps";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import TextField from "@material-ui/core/TextField";
 import { KeyboardDatePicker } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
-import TodayIcon from '@material-ui/icons/Today';
-import MapOutlinedIcon from '@material-ui/icons/MapOutlined';
-import Radio from '@material-ui/core/Radio';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import PinDropOutlinedIcon from '@material-ui/icons/PinDropOutlined';
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import TodayIcon from "@material-ui/icons/Today";
+import MapOutlinedIcon from "@material-ui/icons/MapOutlined";
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import PinDropOutlinedIcon from "@material-ui/icons/PinDropOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
@@ -39,7 +33,7 @@ function App() {
   const [selectedDate, handleDateChange] = useState(new Date());
   const [value, setValue] = React.useState({
     Yes: false,
-    No: false
+    No: false,
   });
   const handleChange = (event) => {
     setDrop(event.target.value);
@@ -49,7 +43,10 @@ function App() {
   };
 
   return (
-    <div className="App" style={{backgroundImage: "linear-gradient(#DBEEFE, white, #DBEEFE)"}}>
+    <div
+      className="App"
+      style={{ backgroundImage: "linear-gradient(#DBEEFE, white, #DBEEFE)" }}
+    >
       <Grid
         container
         direction="row"
@@ -64,12 +61,22 @@ function App() {
             alignItems="flex-start"
           >
             <Grid item xs={3}>
-              <Button variant="contained" color="primary" style={{backgroundColor:"#2A8DC7"}}  size="small">
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#2A8DC7" }}
+                size="small"
+              >
                 Email all users
               </Button>
             </Grid>
             <Grid item xs={2}>
-              <Button variant="contained" color="primary" style={{backgroundColor:"#2A8DC7"}}  size="small">
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#2A8DC7" }}
+                size="small"
+              >
                 see all users
               </Button>
             </Grid>
@@ -81,12 +88,22 @@ function App() {
             alignItems="flex-start"
           >
             <Grid item xs={3}>
-              <Button variant="contained" color="primary" style={{backgroundColor:"#F1F9FF", color:"black"}} size="small">
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#F1F9FF", color: "black" }}
+                size="small"
+              >
                 create customer
               </Button>
             </Grid>
             <Grid item xs={3}>
-              <Button variant="contained" color="primary" style={{backgroundColor:"#F1F9FF", color:"black"}}  size="small">
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#F1F9FF", color: "black" }}
+                size="small"
+              >
                 see recent users
               </Button>
             </Grid>
@@ -119,11 +136,10 @@ function App() {
                   }}
                   value={Drop}
                   onChange={handleChange}
-                 
                   label="select route"
                   select
                 >
-                   <MenuItem value={1}>Route One</MenuItem>
+                  <MenuItem value={1}>Route One</MenuItem>
                   <MenuItem value={2}>Route Two</MenuItem>
                   <MenuItem value={3}>Route Three</MenuItem>
                 </TextField>
@@ -137,7 +153,7 @@ function App() {
             style={{ marginLeft: "15px" }}
           >
             <Grid item>
-              <PinDropOutlinedIcon fontSize='large' />
+              <PinDropOutlinedIcon fontSize="large" />
             </Grid>
             <Grid item>
               <TextField
@@ -158,7 +174,7 @@ function App() {
             style={{ marginLeft: "15px" }}
           >
             <Grid item>
-              <LocationOnOutlinedIcon  fontSize='large'/>
+              <LocationOnOutlinedIcon fontSize="large" />
             </Grid>
             <Grid item>
               <TextField
@@ -179,13 +195,13 @@ function App() {
             alignItems="flex-start"
             style={{ marginLeft: "20px" }}
           >
-            <Grid item> 
-              <TodayIcon style={{ marginTop: "25px" }}/>
+            <Grid item>
+              <TodayIcon style={{ marginTop: "25px" }} />
             </Grid>
-            <Grid item> 
-            <p style={{ marginTop: "25px", fontSize:"18px" }}>Ship Date</p>
+            <Grid item>
+              <p style={{ marginTop: "25px", fontSize: "18px" }}>Ship Date</p>
             </Grid>
-            
+
             <Grid item xs={4}>
               <KeyboardDatePicker
                 autoOk
@@ -208,15 +224,17 @@ function App() {
             alignItems="flex-start"
             style={{ marginLeft: "20px" }}
           >
-            <Grid item> 
-              <TodayIcon style={{ marginTop: "15px" }}/>
+            <Grid item>
+              <TodayIcon style={{ marginTop: "15px" }} />
             </Grid>
-            <Grid item> 
-            <p style={{ marginTop: "15px", fontSize:"18px" }}>Estimated Transit Time :</p>
+            <Grid item>
+              <p style={{ marginTop: "15px", fontSize: "18px" }}>
+                Estimated Transit Time :
+              </p>
             </Grid>
-            
+
             <Grid item xs={4}>
-            <FormControl variant="outlined" className={classes.formControl}>
+              <FormControl variant="outlined" className={classes.formControl}>
                 <TextField
                   id="select"
                   variant="outlined"
@@ -243,19 +261,35 @@ function App() {
             alignItems="flex-start"
             style={{ marginLeft: "20px" }}
           >
-            <Grid item> 
-              <MapOutlinedIcon style={{ marginTop: "5px" }}/>
+            <Grid item>
+              <MapOutlinedIcon style={{ marginTop: "5px" }} />
             </Grid>
-            <Grid item> 
-            <p style={{ marginTop: "5px", fontSize:"18px" }}>Shipment going through Chicago :</p>
+            <Grid item>
+              <p style={{ marginTop: "5px", fontSize: "18px" }}>
+                Shipment going through Chicago :
+              </p>
             </Grid>
-            
+
             <Grid item xs={3}>
-            <RadioGroup  row aria-label="gender" name="gender1" value={value} style={{  marginLeft: "10px" }} onChange={handleChanges}>
-            <FormControlLabel value="Yes" control={<Radio color="primary" />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio color="primary" />} label="No" />
-            </RadioGroup>
-            
+              <RadioGroup
+                row
+                aria-label="gender"
+                name="gender1"
+                value={value}
+                style={{ marginLeft: "10px" }}
+                onChange={handleChanges}
+              >
+                <FormControlLabel
+                  value="Yes"
+                  control={<Radio color="primary" />}
+                  label="Yes"
+                />
+                <FormControlLabel
+                  value="No"
+                  control={<Radio color="primary" />}
+                  label="No"
+                />
+              </RadioGroup>
             </Grid>
           </Grid>
           <Grid
@@ -265,21 +299,27 @@ function App() {
             alignItems="flex-start"
             style={{ marginLeft: "18px" }}
           >
-            <Grid item> 
-            <Button variant="contained" color="primary" style={{backgroundColor:"#F1F9FF", color:"black"}} size="small">
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#F1F9FF", color: "black" }}
+                size="small"
+              >
                 Submit
               </Button>
             </Grid>
-            <Grid item style={{ marginLeft: "20px" }}> 
-            <Button variant="contained" color="primary" style={{backgroundColor:"#F1F9FF", color:"black"}} size="small">
+            <Grid item style={{ marginLeft: "20px" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#F1F9FF", color: "black" }}
+                size="small"
+              >
                 Save Route
               </Button>
             </Grid>
-            
-            
           </Grid>
-         
-              
         </Grid>
         <Grid item xs={6} style={{ backgroundColor: "pink" }}>
           <Maps />
